c9c3f778b9c06feb972def66883e0482a2d5d97c
Manuel Fernandez <manufer6503@gmail.com>
2022-08-30 22:12:22 -0300

    Codigo

--- main.js
+++ main.js
@@ -1,1 +1,25 @@
-asdasdas
+
+// let resultado = 0;
+// let ejecucion;
+
+// do {
+// 	let numeroParaSumar = parseInt(prompt('El resultado es: ' + resultado + ' Escriba el numero que desea sumar'));
+// 	resultado += numeroParaSumar;
+
+// 	let desicion = prompt('Desea seguir sumando numeros? Escriba su respuesta: \n"Si" o "No"');
+// 	desicion.toLowerCase();
+
+// 	if(desicion === "si") {
+// 		ejecucion = true;
+// 	} else if(desicion === "no") {
+// 		ejecucion = false;
+// 		alert('El resultado final es: ' + resultado);
+// 	} else {
+// 		ejecucion = false;
+// 		alert('La respuesta ingresada no es válida. Su programa finalizará.s\nEl resultado final es: ' + resultado);
+// 	}
+// } while(ejecucion)
+
+let numero = parseInt(prompt("Ingresa"));
+alert(numero);
+alert(numero === NaN);
