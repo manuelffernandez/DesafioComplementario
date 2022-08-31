7a8eafcef884f6ec980734914430cf75b8581956
Manuel Fernandez <manufer6503@gmail.com>
2022-08-30 22:16:14 -0300

    descomentar

--- main.js
+++ main.js
@@ -1,25 +1,21 @@
 
-// let resultado = 0;
-// let ejecucion;
+let resultado = 0;
+let ejecucion;
 
-// do {
-// 	let numeroParaSumar = parseInt(prompt('El resultado es: ' + resultado + ' Escriba el numero que desea sumar'));
-// 	resultado += numeroParaSumar;
+do {
+	let numeroParaSumar = parseInt(prompt('El resultado es: ' + resultado + ' Escriba el numero que desea sumar'));
+	resultado += numeroParaSumar;
 
-// 	let desicion = prompt('Desea seguir sumando numeros? Escriba su respuesta: \n"Si" o "No"');
-// 	desicion.toLowerCase();
+	let desicion = prompt('Desea seguir sumando numeros? Escriba su respuesta: \n"Si" o "No"');
+	desicion.toLowerCase();
 
-// 	if(desicion === "si") {
-// 		ejecucion = true;
-// 	} else if(desicion === "no") {
-// 		ejecucion = false;
-// 		alert('El resultado final es: ' + resultado);
-// 	} else {
-// 		ejecucion = false;
-// 		alert('La respuesta ingresada no es válida. Su programa finalizará.s\nEl resultado final es: ' + resultado);
-// 	}
-// } while(ejecucion)
-
-let numero = parseInt(prompt("Ingresa"));
-alert(numero);
-alert(numero === NaN);
+	if(desicion === "si") {
+		ejecucion = true;
+	} else if(desicion === "no") {
+		ejecucion = false;
+		alert('El resultado final es: ' + resultado);
+	} else {
+		ejecucion = false;
+		alert('La respuesta ingresada no es válida. Su programa finalizará.s\nEl resultado final es: ' + resultado);
+	}
+} while(ejecucion)
